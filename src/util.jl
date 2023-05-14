97f3256f9f7c8cec11811cacce0ddb48d237f58e
MilesCranmer <miles.cranmer@gmail.com>
2023-05-14 16:14:44 +0100

    Create `@loop_guard` macro to prevent infinite loops

--- src/util.jl
+++ src/util.jl
@@ -3,6 +3,33 @@ using Sockets
 export log, getsocket, @log
 import Base: show_unquoted
 
+const MAX_LOOPS = 2^16
+
+macro loop_guard(ex)
+    @assert ex.head == :while
+    loop_contents = ex.args[2]
+    @assert loop_contents.head == :block
+
+    loop_counter = gensym()
+    source = QuoteNode(__source__)
+
+    # ex is a while loop, so we insert this loop counter at the end,
+    # with a check to make sure it doesn't exceed MAX_LOOPS:
+    push!(loop_contents.args, :($loop_counter += 1))
+    push!(
+        loop_contents.args,
+        :(
+            $loop_counter > $MAX_LOOPS &&
+            (@warn("Infinite loop detected at " * string($source) * ". Exiting."); break)
+        ),
+    )
+
+    return esc(quote
+        local $loop_counter = 0
+        $ex
+    end)
+end
+
 macro log(exs...)
     blk = Expr(:block)
     loc = string("\t", __source__.file, "#", __source__.line)
