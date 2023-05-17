28c53a40c2f8e022733b40780aa8941c13ccd001
Caleb Allen <caleb@wim.xyz>
2023-05-17 09:58:37 +0100

    Remove unused log methods

--- src/util.jl
+++ src/util.jl
@@ -1,7 +1,5 @@
 module Util
-using Sockets
-export @debug, getsocket, @debug, @loop_guard
-import Base: show_unquoted
+export @loop_guard
 
 const MAX_LOOPS = 2^16
 
@@ -29,85 +27,4 @@ macro loop_guard(ex)
         $ex
     end)
 end
-
-macro @debug(exs...)
-    blk = Expr(:block)
-    loc = string("\t", __source__.file, "#", __source__.line)
-    for ex in exs
-        push!(blk.args, :(println(getsocket(),
-            $(sprint(show_unquoted, ex) * " = "),
-            repr(begin
-                value = $(esc(ex))
-            end),
-            "\n",
-            $loc,
-            "\n",
-        )))
-    end
-    isempty(exs) || push!(blk.args, :value)
-    return blk
-end
-
-# macro @debug(str)
-#     blk = Expr(:block)
-#     loc = string("\t", __source__.file, "#", __source__.line)
-#     push!(blk.args, :(println(getsocket(),
-#                               # repr(begin value=$(esc(str)) end),
-#                               $str,
-#                               "\n",
-#                               $loc,
-#                               "\n",
-#                               )))
-#     push!(blk.args, :value)
-#     return blk
-# end
-
-
-# TODO this is terrible to overload
-function Base.@debug(s...)
-    println(getsocket(), s...)
-end
-
-function __init__()
-    global socket = devnull
-end
-function enable_logging()
-    global socket = connect(1234)
-end
-
-function getsocket()
-    if @isdefined(socket)
-        return socket
-    end
-    return devnull
-end
-
-function Base.@debug(any::Any)
-    socket = getsocket()
-    println(socket, any)
-end
-
-
-function test_bind()
-
-end
-
-# 'j' => (s::LE.MIState, o...)->LE.edit_move_down(s),
-
-macro bindkey(c)
-    # char = ($(esc(c)))
-    # char = esc(c)
-    # @show sym = Symbol(char)
-    return :(
-        () -> eval(Expr(:call, Symbol($(esc(c))))))
-    # pair item 2
-    # (s::LE.MIState, o...)->begin
-    # @show Symbol($(esc(c)))
-    # Expr(:call, Symbol($(esc(c))),(VB.mode, esc(s)))
-end
-
-# buffer(s :: LE.MIState) = LE.buffer(s)
-
-# alpha_keymap = AnyDict()
-
 end
