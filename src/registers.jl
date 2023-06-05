784d61c4a17a22fdfeea9b5ffba7cf6aeea05243
caleb-allen <caleb.e.allen@gmail.com>
2023-06-05 14:02:59 +0100

    Add notes to Registers

--- src/registers.jl
+++ src/registers.jl
@@ -1,3 +1,19 @@
+"""
+From vim :h registers:
+
+There are ten types of registers:		*registers* *{register}* *E354*
+1. The unnamed register ""
+2. 10 numbered registers "0 to "9
+3. The small delete register "-
+4. 26 named registers "a to "z or "A to "Z
+5. Three read-only registers ":, "., "%
+6. Alternate buffer register "#
+7. The expression register "=
+8. The selection registers "* and "+
+9. The black hole register "_
+10. Last search pattern register "/
+
+"""
 module Registers
 
 export get
