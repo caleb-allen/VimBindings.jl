2ca5875c8fe3387535688ab080b4d5e54481db63
Caleb Allen <caleb.e.allen@gmail.com>
2023-07-18 19:00:01 +0100

    Add documentation with Documenter (#78)

---
+++ docs/make.jl
@@ -0,0 +1,22 @@
+using Documenter
+using VimBindings
+
+makedocs(
+    sitename = "VimBindings",
+    modules = [VimBindings],
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    ),
+    pages = [
+        "index.md",
+        "features.md"
+    ]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+deploydocs(
+    repo = "github.com/caleb-allen/VimBindings.jl.git",
+    push_preview=true
+)
